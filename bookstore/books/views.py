97a2f83ddc4de1e64248730a616f315ed9af79a1
Aniket Kadam <kaniket457@gmail.com>
2023-05-16 20:10:00 +0530

    define the views for listing and displaying books

--- bookstore/books/views.py
+++ bookstore/books/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
 # Create your views here.
+from .models import Book
+
+def book_list(request):
+    books = Book.objects.all()
+    return render(request, 'books/book_list.html', {'books': books})
+
+def book_detail(request, pk):
+    book = get_object_or_404(Book, pk=pk)
+    return render(request, 'books/book_detail.html', {'book': book})
