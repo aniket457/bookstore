01fc72fc0774f4464fd26189b8f9aca50e704d57
Aniket Kadam <kaniket457@gmail.com>
2023-05-18 19:38:45 +0530

    modified views for listing and displaying books

--- bookstore/books/views.py
+++ bookstore/books/views.py
@@ -5,8 +5,8 @@ from .models import Book
 
 def book_list(request):
     books = Book.objects.all()
-    return render(request, 'books/book_list.html', {'books': books})
+    return render(request, 'book_list.html', {'books': books})
 
 def book_detail(request, pk):
     book = get_object_or_404(Book, pk=pk)
-    return render(request, 'books/book_detail.html', {'book': book})
+    return render(request, 'book_detail.html', {'book': book})
